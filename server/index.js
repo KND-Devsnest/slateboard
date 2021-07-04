d44e9dcac448efee6e7cd86fb3c0199649f2d4ad
aguyran <59661860+aguyran@users.noreply.github.com>
2021-07-04 15:35:31 +0530

    iniital commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var socket = require("socket.io");
+var app = express();
+app.use(express.static("public"));
+var server = app.listen(8000);
+var io = socket(server);
+io.sockets.on("connection", newConnection);
+const drawings = [];
+function newConnection(socket) {
+  console.log("new connection" + socket.id);
+  socket.broadcast.emit("hello");
+  console.log(drawings);
+  socket.on("mouse", mouseMsg);
+  function mouseMsg(data) {
+    drawings.push(data);
+    socket.broadcast.emit("mouse", data);
+  }
+}
