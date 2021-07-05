8221e32f179b1b641cd9b818eecffabd3bdc3b90
seebham <shubhamisdev@gmail.com>
2021-07-05 17:47:18 +0530

    directory structure fixed

--- public/app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'),
-    fs = require('fs'),
-    path = require('path');
-
-http.createServer(function(req, res, next) {
-    var filePath = path.join(__dirname, 'index.html');
-    const file = fs.readFileSync(filePath);
-    res.end(file);
-}).listen(3000);
